47ad9493ae6b577d7e0cf408a979080e15ec544c
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-01-13 11:41:54 -0900

    Companion Creation API Done

--- app/(root)/companion/_components/companion-form.tsx
+++ app/(root)/companion/_components/companion-form.tsx
@@ -12,6 +12,9 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import axios from "axios"
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -57,6 +60,9 @@ const formSchema = z.object({
 }) 
 
 export const CompanionForm = ({initialData, categories}: CompanionFormProps) => {
+    const router = useRouter()
+
+    const { toast } = useToast()
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -71,8 +77,25 @@ export const CompanionForm = ({initialData, categories}: CompanionFormProps) => 
 
     const isLoading = form.formState.isSubmitting
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            if(initialData) {
+                await axios.patch(`/api/companion/${initialData.id}`, values)
+            } else {
+                await axios.post(`/api/companion`, values)
+            }
+            toast({
+                description: "Success!"
+            })
 
+            router.refresh()
+            router.push("/")
+        } catch (error) {
+            toast({
+                variant: "destructive",
+                description: "Something went wrong!"
+            })
+        }
     }
 
     return (
