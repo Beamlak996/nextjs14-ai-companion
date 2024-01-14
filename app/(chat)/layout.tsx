72f3454aee48563cb8301f3567710380eb3ffa7a
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-01-14 01:29:46 -0900

    Chat Header Done

---
+++ app/(chat)/layout.tsx
@@ -0,0 +1,10 @@
+
+const ChatLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className="h-full w-full max-w-4xl mx-auto" >
+        {children}
+    </div>
+  )
+}
+
+export default ChatLayout
