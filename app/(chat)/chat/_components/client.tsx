a27d20cf49072e63a56680c07084ee604f460514
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-01-15 09:23:20 -0900

    Chat Form Done

--- app/(chat)/chat/_components/client.tsx
+++ app/(chat)/chat/_components/client.tsx
@@ -1,7 +1,11 @@
-"use client"
+"use client";
 
 import { ChatHeader } from "@/components/chat-header";
-import { Companion, Message } from "@prisma/client"
+import { Companion, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { useCompletion } from "ai/react"
+import { ChatForm } from "@/components/chat-form";
 
 type ChatClientProps = {
   companion: Companion & {
@@ -12,10 +16,50 @@ type ChatClientProps = {
   };
 };
 
-export const ChatClient = ({companion}: ChatClientProps) => {
-    return (
-        <div className="flex flex-col h-full p-4 space-y-2" >
-            <ChatHeader companion={companion} />
-        </div>
-    )
-}
+export const ChatClient = ({ companion }: ChatClientProps) => {
+  const [messages, setMessages] = useState<any[]>(companion.messages) 
+
+  const router = useRouter()
+
+  const { input, isLoading, handleInputChange, handleSubmit, setInput } =
+    useCompletion({
+      api: `/api/chat/${companion.id}`,
+      onFinish(_prompt, completion) {
+        const systemMessage = {
+          role: "system",
+          content: completion,
+        };
+
+        setMessages((current) => [...current, systemMessage]);
+        setInput("");
+
+        router.refresh();
+      },
+    });
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+      const userMessage = {
+        role: "user",
+        content: input,
+      };
+
+      setMessages((current) => [...current, userMessage]);
+
+      handleSubmit(e);
+    };
+
+  return (
+    <div className="flex flex-col h-full p-4 space-y-2">
+      <ChatHeader companion={companion} />
+      <div>
+        Messages TODO
+      </div>
+      <ChatForm 
+        isLoading={isLoading}
+        input={input}
+        handleInputChange={handleInputChange}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
